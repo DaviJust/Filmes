e0509d186d05367df71eb1270d8f7613e40d73e6
Davi <davijustinocouto@gmail.com>
2023-01-24 20:27:34 -0300

    Github pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,19 @@ import {BiCameraMovie} from 'react-icons/bi'
 import { useState } from 'react'
 
 function Header(){
+    let loginOn = localStorage.getItem('logado');
     return(
         <div className={styles.headerDiv}>
-            <Link to='/Home'>
+            <Link to='/'>
                 <h1 className={styles.text}>{<BiCameraMovie className={styles.iconText}/>} React Flix</h1>
             </Link>
             <ul>
                 <Link to='/Home'>Home</Link>
-                <Link to='/Login'>Login</Link>
                 <Link to='/Filmes'>Filmes</Link>
                 <Link to='/Sobre'>Sobre</Link>
                 <Link to='/Contato'>Contato</Link>
+                <Link to='/Login' className={styles.login}>Login</Link>
+                
                 {/* <Link to='/PostFilme'>Adicionar</Link> */}
                 
             </ul>
